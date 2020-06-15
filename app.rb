97bda726ee4d526d9c3b36b1bacec9f3ab7ad1cf
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-15 21:34:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,15 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-    "My name is "
+    "My name is Josh "
   end
 
   get '/hometown' do
-    "My hometown is "
+    "My hometown is Denver "
   end
 
   get '/favorite-song' do
-    "My favorite song is "
+    "My favorite song is Last Resort by Papa Roach"
   end
 
 
